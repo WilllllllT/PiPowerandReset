f68908a73d4a2cae9e75c2e1e2d3dff93719701f
Will-Terry-Uni <william.terry.24@ucl.ac.uk>
2024-11-16 17:28:55 +0000

    made the escape press rather then full reboot

--- listen-for-reset.py
+++ listen-for-reset.py
@@ -8,4 +8,4 @@ GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.wait_for_edge(4, GPIO.FALLING)
 
 # Simulate ESC key press using ASCII code 27
-subprocess.call(['xdotool', 'key', 'Escape'])  # Simulate ESC key press
+subprocess.call(['sudo', 'sh', '-c', 'echo -ne "\\033" > /dev/input/event0'])  # Send ESC keycode directly to input device
