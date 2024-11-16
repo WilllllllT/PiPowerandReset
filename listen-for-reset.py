27f516c8c6616d403b177794cc9264ea4371db33
Will-Terry-Uni <william.terry.24@ucl.ac.uk>
2024-11-16 17:47:29 +0000

    last test before commiting to full reboot

--- listen-for-reset.py
+++ listen-for-reset.py
@@ -7,13 +7,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.wait_for_edge(4, GPIO.FALLING)
 
-# Simulate ESC key press using keyboard events
-try:
-    # Write ESC key code directly to input device
-    with open('/dev/input/event0', 'wb') as f:
-        # ESC key code is 1
-        f.write(bytes([0x01, 0x00, 0x00, 0x00])) 
-except PermissionError:
-    print("Error: Need root permissions to simulate keyboard events")
+# Reboot the system
+subprocess.call(['xdotool', 'key', 'Escape'], shell=False)
 
 
