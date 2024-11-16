c7539317415733b59628cebf9f2b96b2ec500a1c
Will-Terry-Uni <william.terry.24@ucl.ac.uk>
2024-11-16 17:14:57 +0000

    updated reboot

--- listen-for-reset.py
+++ listen-for-reset.py
@@ -2,23 +2,9 @@
 
 import RPi.GPIO as GPIO
 import subprocess
-from evdev import UInput, ecodes
 
-# Setup GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.wait_for_edge(4, GPIO.FALLING)
 
-try:
-    # Wait for button press
-    GPIO.wait_for_edge(4, GPIO.FALLING)
-    
-    # Create a virtual keyboard device
-    with UInput() as ui:
-        # Simulate pressing and releasing ESC key
-        ui.write(ecodes.EV_KEY, ecodes.KEY_ESC, 1)  # Press ESC
-        ui.write(ecodes.EV_KEY, ecodes.KEY_ESC, 0)  # Release ESC
-        ui.syn()
-    
-finally:
-    # Clean up GPIO on exit
-    GPIO.cleanup()
+subprocess.call(['reboot'], shell=False)
