d8c2f2793b9893381e0b06ce40a037f1109c29ec
Will-Terry-Uni <william.terry.24@ucl.ac.uk>
2024-11-16 17:42:37 +0000

    added the ESC key press a different way usnig uniput

--- listen-for-reset.py
+++ listen-for-reset.py
@@ -7,11 +7,13 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.wait_for_edge(4, GPIO.FALLING)
 
-# Simulate ESC key press using uinput
+# Simulate ESC key press using keyboard events
 try:
-    # Use xdotool to simulate ESC key press
-    subprocess.call(['xdotool', 'key', 'Escape'])
-except FileNotFoundError:
-    print("Please install xdotool: sudo apt-get install xdotool")
+    # Write ESC key code directly to input device
+    with open('/dev/input/event0', 'wb') as f:
+        # ESC key code is 1
+        f.write(bytes([0x01, 0x00, 0x00, 0x00])) 
+except PermissionError:
+    print("Error: Need root permissions to simulate keyboard events")
 
 
